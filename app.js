66a2ea900a8ba937052f5666d957f4db2d915af9
UpUp <2252292157@qq.com>
2020-12-08 15:20:52 +0800

    "Full pipeline test"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Amazon Cloud!\noh my god!'));
+app.get('/', (req, res) => res.send('Hello Amazon Cloud!\nhhh'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
