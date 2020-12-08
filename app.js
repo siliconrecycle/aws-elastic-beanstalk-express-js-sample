b989f6274aeda9fa614a7c3868f367cb1a992585
UpUp <2252292157@qq.com>
2020-12-08 15:27:56 +0800

    "branch test"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Amazon Cloud!\nhhh'));
+app.get('/', (req, res) => res.send('Hello Amazon Cloud!\nhhh\n In development branch!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
